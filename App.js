c35ba5c88d9008ac9f9af89417d0abe481329cd8
valleDD <mvalledeniz@gmail.com>
2024-04-28 11:01:59 +0100

    feat: subir imagen logo, añadir fuente,subir imagenes y modificar los componente para que sean mas visuales

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import HomeScreen from './Component/HomeScreen';
-import ClientHomeScreen from './Component/ClientHomeScreen';
 import LoginScreen from './Component/LoginScreen';
 import RegisterScreen from './Component/RegisterScreen';
+
 import BakeryHomeScreen from './Component/BakeyHomeScreen';
-import OrdersEarringsScreen from './Component/OrdersEarringsScreen';
+import ClientHomeScreen from './Component/ClientHomeScreen';
+
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-    <OrdersEarringsScreen/>
-    </View>
+    <RegisterScreen/>
   );
 }
 
